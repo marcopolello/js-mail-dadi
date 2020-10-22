bc27325a96856a05f4ca04df54fb883233bf6434
marcopolello <marco-pol@hotmail.it>
2020-10-22 14:27:00 +0200

    creazione ragionamento

---
+++ mail/js/script.js
@@ -0,0 +1,15 @@
+// esercizio MAIL
+// Chiedi all’utente la sua email
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// creo una lista di mail tramite un array
+var lista = ["marco@gmail.com", "davide@gmail.com", "nicolò@gmail.com", "john@gmail.com", "simone@gmail.com", "edoardo@gmail.com"];
+
+// chiedo all'utente la sua mail tramite un form
+var check = document.getElementById('id');
+check.addEventListener('click', function(){
+
+
+
+});
