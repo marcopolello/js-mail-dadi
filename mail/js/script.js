57ff3d0df991d87d282a06b2b314f0619c4377bd
marcopolello <marco-pol@hotmail.it>
2020-10-22 18:15:28 +0200

    ragionamento ok

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,22 +6,28 @@
 // creo una lista di mail tramite un array
 var lista = ["marco@gmail.com", "davide@gmail.com", "nicolò@gmail.com", "john@gmail.com", "simone@gmail.com", "edoardo@gmail.com"];
 
-for (var i = 0; i < lista.length; i++) {
-  var item = lista[i];
-  console.log(item);
-}
-
 // CHIEDO ALL'UTENTE LA SUA MAIL TRAMITE UN FORM
 // genero una variabile che scatena una funzione successivamente al click
 var check = document.getElementById('controlla');
 // al click di questo input creo una funzione
 check.addEventListener('click',
-function(){
-    //mail data da input utente
-    var mail = document.getElementById('indirizzo').value;
-    // guardo se la mail è uguale a uno degli elementi della lista array
-    if (mail === item) {
-      console.log(mail);
-      document.getElementById('mail_ok').innerHTML = "la tua mail è corretta";
+function()
+{
+  //mail data da input utente
+  var mail = document.getElementById('indirizzo').value;
+  var verifica = false;
+  for (var i = 0; i < lista.length; i++)
+  {
+    var item = lista[i];
+    console.log(item);
+
+    if (mail == item)
+    {
+      verifica = true;
     }
-});
+  }
+  if (verifica == true) {
+    document.getElementById('mail_ok').innerHTML = "la tua mail è corretta";
+  } else {document.getElementById('mail_ok').innerHTML = "la tua mail non è corretta";}
+}
+);
