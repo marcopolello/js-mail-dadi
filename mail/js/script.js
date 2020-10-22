858ee6a0ff36d6ea0d959d06e7c2d86c72fb962c
marcopolello <marco-pol@hotmail.it>
2020-10-22 15:41:10 +0200

    funziona solo con ultima mail

--- mail/js/script.js
+++ mail/js/script.js
@@ -6,10 +6,22 @@
 // creo una lista di mail tramite un array
 var lista = ["marco@gmail.com", "davide@gmail.com", "nicolò@gmail.com", "john@gmail.com", "simone@gmail.com", "edoardo@gmail.com"];
 
-// chiedo all'utente la sua mail tramite un form
-var check = document.getElementById('id');
-check.addEventListener('click', function(){
+for (var i = 0; i < lista.length; i++) {
+  var item = lista[i];
+  console.log(item);
+}
 
-
-
+// CHIEDO ALL'UTENTE LA SUA MAIL TRAMITE UN FORM
+// genero una variabile che scatena una funzione successivamente al click
+var check = document.getElementById('controlla');
+// al click di questo input creo una funzione
+check.addEventListener('click',
+function(){
+    //mail data da input utente
+    var mail = document.getElementById('indirizzo').value;
+    // console.log(mail);
+    // guardo se la mail è uguale a uno degli elementi della lista array
+    if (mail === item) {
+      document.getElementById('mail_ok').innerHTML = "la tua mail è corretta";
+    } 
 });
